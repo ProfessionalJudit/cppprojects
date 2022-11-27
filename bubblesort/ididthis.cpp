f9da36dea6a9d3ef0951db526153c4fad8439bfb
Judit <juditclosa@gmail.com>
2022-11-27 22:51:40 +0100

    Comitoooo

---
+++ bubblesort/ididthis.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <chrono>
+using namespace std::chrono;
+
+void sort(int array[], int size);
+
+int main(int argc, char const *argv[])
+{
+    auto start = high_resolution_clock::now();
+    int array[]{5, 4, 2, 1, 7};
+    sort(array, sizeof(array) / sizeof(int));
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    // To get the value of duration use the count()
+    // member function on the duration object
+    std::cout << duration.count() << std::endl;
+    return 0;
+}
+
+void sort(int array[], int size)
+{
+    bool sorted = true;
+    int carry{0};
+    std::cout << sizeof(int) << std::endl;
+    std::cout << "<------------------->" << std::endl;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        sorted = true;
+        for (size_t j = 0; j < size - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                carry = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = carry;
+                sorted = false;
+            }
+        }
+        if (sorted)
+        {
+            break;
+        }
+
+        std::cout << "[ ";
+        for (size_t j = 0; j < size; j++)
+        {
+            std::cout << array[j] << " ";
+        }
+        std::cout << "]" << std::endl;
+    }
+    std::cout << "<------------------->" << std::endl;
+}
---
+++ bubblesort/mybfdidthis.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <chrono>
+using namespace std::chrono;
+
+int main() {
+    auto start = high_resolution_clock::now();
+    int array[]{5, 4, 2, 1, 7};
+    
+    int carry;
+    bool sort;
+    for(int i = 1; i < sizeof(array)/sizeof(int); i++){
+        sort = true;
+        for(int j = i; j < sizeof(array)/sizeof(int); j++){
+            if (array[i - 1] > array[j]){
+                carry = array[i - 1];
+                array[i - 1] = array[j];
+                array[j] = carry;
+                sort = false;
+            }
+        }
+        if(sort) break;
+    }
+    std::cout << std::endl << "{ ";
+    for(int j = 0; j < sizeof(array)/sizeof(int); j++) std::cout << array[j] << " ";    
+    std::cout << "}";
+        auto stop = high_resolution_clock::now();
+auto duration = duration_cast<microseconds>(stop - start);
+ 
+// To get the value of duration use the count()
+// member function on the duration object
+std::cout << duration.count() << std::endl;
+
+    return 0;
+}
